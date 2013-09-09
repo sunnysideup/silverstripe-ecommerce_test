2c15166bbbc934be00f28fed34abc48ea8aa8dd3
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2013-09-09 05:28:36 +0000

    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/ecommerce_test/trunk@5226 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- mysite/_config.php
+++ mysite/_config.php
@@ -4,19 +4,9 @@ global $project;
 $project = 'mysite';
 
 global $database;
-$database = 'kleinekeuken_nl';
+$database = 'ecommerce_test';
 
 require_once('conf/ConfigureFromEnv.php');
-
-MySQLDatabase::set_connection_charset('utf8');
-
-// Set the current theme. More themes can be downloaded from
-// http://www.silverstripe.org/themes/
-SSViewer::set_theme('simple');
-
-// Enable nested URLs for this site (e.g. page/sub-page/)
-if(class_exists('SiteTree')) SiteTree::enable_nested_urls();
-
 
 Email::setAdminEmail("sales@silverstripe-ecommerce.com");
 
