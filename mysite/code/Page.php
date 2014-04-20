998efd3b6cd9910495ced57e9b8d8408486c5e93
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2014-04-20 02:15:30 +0000

    Adding additional metatags
    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/ecommerce_test/trunk@5625 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -93,5 +93,31 @@ class Page_Controller extends ContentController {
 		}
 	}
 
+	function extendedMetaTags(){
+		return "
+			<base href=\"".Director::baseURL()."\" />
+			<meta charset=\"utf-8\" />
+			<meta http-equiv=\"Content-type\" content=\"text/html; charset=utf-8\" />
+			<title>".$this->Title."</title>
+			<link rel=\"icon\" href=\"".Director::baseURL()."/favicon.ico\" type=\"image/x-icon\" />
+			<link rel=\"shortcut icon\" href=\"".Director::baseURL()."/favicon.ico\" type=\"image/x-icon\" />
+			<meta name=\"robots\" content=\"NOODP, all, index, follow\" />
+			<meta name=\"googlebot\" content=\"NOODP, all, index, follow\" />
+			<meta name=\"rights\" content=\"Sunny Side Up\" />
+			<meta name=\"created\" content=\"".$this->Created."\" />
+			<meta name=\"geo.country\" content=\"New Zealand\" />
+			<meta http-equiv=\"imagetoolbar\" content=\"no\" />
+			<meta name=\"viewport\" content=\"width=device-width,initial-scale=1\" />
+			<meta http-equiv=\"Content-Language\" content=\"en_NZ\" />
+
+			<meta property=\"og:title\" content=\"".$this->Title."\" />
+			<meta property=\"og:type\" content=\"website\" />
+			<meta property=\"og:url\" content=\"".$this->Link()."/\" />
+			<meta property=\"og:site_name\" content=\"Silverstripe E-commerce\" />
+			<meta property=\"og:description\" content=\"".substr(strip_tags($this->Content),0, 100)."\" />
+
+		";
+	}
+
 }
 
