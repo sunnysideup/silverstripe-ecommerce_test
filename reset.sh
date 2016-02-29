896e417ebc72435a481dff4d83123563ec03370a
Nicolaas <github@sunnysideup.co.nz>
2016-03-01 10:03:34 +1300

    adding script

--- reset.sh
+++ reset.sh
@@ -1,1 +1,5 @@
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+php framework/cli-script.php dev/tasks/CleanEcommerceTables
+php framework/cli-script.php dev/tasks/DefaultRecordsForEcommerce
