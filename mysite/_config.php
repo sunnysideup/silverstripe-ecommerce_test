a3f43e2235d02b78bd98d11790287e2a9d21ae03
Sunny Side Up <github@sunnysideup.co.nz>
2018-04-09 16:33:44 +1200

    PATCH: force SSL

--- mysite/_config.php
+++ mysite/_config.php
@@ -10,6 +10,16 @@ require_once('conf/ConfigureFromEnv.php');
 
 /*** ECOMMERCE ***/
 
+if(Director::isLive()) {
+        Director::forceSSL();
+        SS_Log::add_writer(new SS_LogEmailWriter('errors@sunnysideup.co.nz'), SS_Log::ERR);
+}
+else {
+//      BasicAuth::protect_entire_site();
+        if(Director::isDev()) {
+                SSViewer::set_source_file_comments(true);
+        }
+}
 
 $theme = Session::get("theme");
 if (!$theme) {
