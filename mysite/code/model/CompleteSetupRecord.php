2d56d4efb8cbb5ad375b96624c4c214adae06657
Nicolaas Thiemen <nt@sunnysideup.co.nz>
2020-06-05 14:23:32 +1200

    MAJOR: upgrade to new version of Silverstripe - step: Add the table name

--- mysite/code/model/CompleteSetupRecord.php
+++ mysite/code/model/CompleteSetupRecord.php
@@ -2,6 +2,20 @@
 
 class CompleteSetupRecord extends DataObject
 {
+
+/**
+  * ### @@@@ START REPLACEMENT @@@@ ###
+  * OLD: private static $db (case sensitive)
+  * NEW: 
+    private static $table_name = '[SEARCH_REPLACE_CLASS_NAME_GOES_HERE]';
+
+    private static $db (COMPLEX)
+  * EXP: Check that is class indeed extends DataObject and that it is not a data-extension!
+  * ### @@@@ STOP REPLACEMENT @@@@ ###
+  */
+    
+    private static $table_name = 'CompleteSetupRecord';
+
     private static $db = array(
         "CompletedSetup" => "Boolean"
     );
